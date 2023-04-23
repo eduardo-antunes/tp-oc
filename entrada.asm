7d12b45c5de789d9f2c22a269b15f04a3aa689f0
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2023-04-23 14:09:05 -0300

    Adiciona assembling do formato S

--- entrada.asm
+++ entrada.asm
@@ -2,3 +2,4 @@ sub  x5, x6, x0
 andi x3, x4, 2
 or   x3, x4, x5
 lh   x6, 0(x9)
+sh   x7, 0(x8)
--- ras/main.py
+++ ras/main.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
+def rshift(val, n):
+    return val >> n if val >= 0 else -val >> n
+
 # Funções de montagem: as funções abaixo realizam a montagem de cada
 # instrução, utilizando os valores dados como argumentos
 
 def assemble_rtype(opcode, funct3, funct7, rd, rs1, rs2):
+    # Formato R (registradores)
     print(f"{funct7:07b}{rs2:05b}{rs1:05b}{funct3:03b}{rd:05b}{opcode:07b}")
 
 def assemble_itype(opcode, funct3, rd, rs1, number):
+    # Formato I (imediato)
     print(f"{number:012b}{rs1:05b}{funct3:03b}{rd:05b}{opcode:07b}")
+
+def assemble_stype(opcode, funct3, rs1, rs2, number):
+    # Formato S (store)
+    n4_0 = number & 0b11111
+    n11_5 = rshift(number, 5)
+    print(f"{n11_5:07b}{rs2:05b}{rs1:05b}{funct3:03b}{n4_0:05b}{opcode:07b}")
+
 
 # Funções de processamento de texto: os argumentos no arquivo fonte são
 # dados em forma textual e vêm em diferentes formatos. Para cada formato,
@@ -29,6 +41,15 @@ def get_rri(args):
     imm = int(rri[2])
     return rd, rs1, imm
 
+# RRS: reg, imediato(reg)
+def get_rrs(args):
+    parts = args.split(',')
+    rs1 = int(parts[0][1:])
+    others = parts[1][:-1].split('(') # )
+    imm = int(others[0])
+    rs2 = int(others[1][1:])
+    return rs1, rs2, imm
+
 # Função principal:
 
 def main(argv):
@@ -48,11 +69,17 @@ def main(argv):
 
             if instruction == "lh":
                 # lh: Load Half-word, passa 16 bits da RAM para um registrador
-                pass
+                funct3 = 0b001
+                opcode = 0b0000011
+                rd, rs1, number = get_rrs(others)
+                assemble_itype(opcode, funct3, rd, rs1, number)
 
             elif instruction == "sh":
                 # sh: Store Half-word, passa 16 bits de um registrador para RAM
-                pass
+                funct3 = 0b001
+                opcode = 0b0100011
+                rs1, rs2, number = get_rrs(others)
+                assemble_stype(opcode, funct3, rs1, rs2, number)
 
             elif instruction == "sub":
                 # sub: Subtract, subtrai rs2 de rs1 e coloca o resultado em rd
